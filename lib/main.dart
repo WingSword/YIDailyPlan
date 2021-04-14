a236678df0bdffb3932ccd11b2364eb094ece5ff
程梦楠 <mengnancheng@yunzhijia.com>
2021-04-14 10:34:36 +0800

    first git

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('YIDailyPlan'),
+        ),
+        body: new Center(
+          child: new Text(
+              '1\n'
+              '2'
+              '3\n'
+              '4'),
+        ),
+      ),
+    );
+  }
+}
